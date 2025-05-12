1905069396a5fce07db9295d0f6660b0b98fac6c
Filip Jinneklint <filip.jinneklint@elev.ga.ntig.se>
2025-05-12 09:03:25 +0200

    hello

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.io.*;
+import java.net.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String args[]) {
+        Socket client = null;
+
+        int portnumber = 2;
+        if (args.length >= 1) {
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            try {
+                String msg = "";
+
+                client = new Socket(InetAddress.getLocalHost(), portnumber);
+                System.out.println("Client socket is created: " + client);
+
+                OutputStream clientOut = client.getOutputStream();
+                PrintWriter pw = new PrintWriter(clientOut, true);
+
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("Skriv ett matteuttryck (t.ex. 3+4). Skriv Bye för att avsluta.");
+
+                msg = stdIn.readLine().trim();
+                pw.println(msg);
+
+                System.out.println("Svar från servern: " + br.readLine());
+
+                pw.close();
+                br.close();
+                client.close();
+
+                if (msg.equalsIgnoreCase("Bye")) {
+                    break;
+                }
+
+            } catch (IOException ie) {
+                System.out.println("I/O error: " + ie);
+            }
+        }
     }
 }
